c392773baf3c9156c105549c2c01bf57c77c6682
Razvan Rosca <razvan.rosca@gmail.com>
2015-07-23 23:48:45 +0300

    Update exim-flush-wizzard.sh

--- email/exim-flush-wizzard.sh
+++ email/exim-flush-wizzard.sh
@@ -1,4 +1,7 @@
 read -p "Enter domain: " domain;
-printf %s "Flushing emails for $domain ..."
-exim -bp|grep $domain|awk '{print $3}' |xargs exim -Mrm >/dev/null 2>&1
+printf %s "Flushing emails FROM $domain ..."
+exiqgrep -if $domain | xargs exim -Mrm >/dev/null 2>&1
+printf '%s\n' "DONE!"
+printf %s "Flushing emails TO $domain ..."
+exiqgrep -ir $domain | xargs exim -Mrm >/dev/null 2>&1
 printf '%s\n' "DONE!"

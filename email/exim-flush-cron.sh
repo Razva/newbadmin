5bcaaa9d1dd7f5e5089b1060a2209def3fe8a77e
Razvan Rosca <razvan.rosca@gmail.com>
2015-07-24 01:01:18 +0300

    Update exim-flush-cron.sh

--- email/exim-flush-cron.sh
+++ email/exim-flush-cron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Before running this script you NEED to setup /etc/blockeddomains
 # as instructed at http://www.linuxbrigade.com/block-account-sending-mail-whmcpanel/
-while read line; do exiqgrep -if ${line} | xargs exim -Mrm &> /dev/null; done < /etc/blockeddomains &> /dev/null
-while read line; do exiqgrep -ir ${line} | xargs exim -Mrm &> /dev/null; done < /etc/blockeddomains &> /dev/null
-exim -bp|grep frozen|awk '{print $3}' |xargs exim -Mrm &> /dev/null
+while read line; do /usr/sbin/exiqgrep -if ${line} | xargs /usr/sbin/exim -Mrm &> /dev/null; done < /etc/blockeddomains &> /dev/null
+while read line; do /usr/sbin/exiqgrep -ir ${line} | xargs /usr/sbin/exim -Mrm &> /dev/null; done < /etc/blockeddomains &> /dev/null
+/usr/sbin/exim -bp|grep frozen|awk '{print $3}' |xargs /usr/sbin/exim -Mrm &> /dev/null

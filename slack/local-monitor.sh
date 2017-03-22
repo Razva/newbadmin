7950adf6e39ac961e692cb4cd11d168ed704383f
Razvan Rosca <razvan.rosca@gmail.com>
2017-03-22 19:48:51 +0200

    Update local-monitor.sh

--- slack/local-monitor.sh
+++ slack/local-monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Special thanks to koala_man @ FreeNode
+# Requirements: JQ should be installed. For CentOS "yum install jq", for Ubuntu "apt-get install jq".
 
 # Configuration
 file="/some/file.log"
@@ -7,7 +8,8 @@ hook="https://hooks.slack.com/services/YOUR-HOOK"
 ignores="ignored_keyword|another_ignored_keyword" # split multiple ignores with pipes
 ### STOP EDITING ###
 
-tail -n0 -F "$file" | while read LINE; do
-  (echo "$LINE" | grep -vE -e "$ignores") && curl -X POST --silent --data-urlencode \
-    "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$hook";
+export LC_ALL=C
+tail -n0 -F "$file" | while IFS= read -r line; do
+  (printf '%s\n' "$line" | grep -vE -e "$ignores") && curl -X POST --silent --data-urlencode \
+    "payload={\"text\": $(jq -sR '.' <<< "$line" ) }" "$hook";
 done

2c52ad915ebe095913cfaf1b94a137e37cda2b77
Razvan Rosca <razvan.rosca@gmail.com>
2017-03-31 09:26:21 +0300

    Create reset-local-ips.pl

---
+++ cpanel/reset-local-ips.pl
@@ -0,0 +1,11 @@
+#!/usr/local/cpanel/3rdparty/bin/perl
+
+use Cpanel::Config::Users          ();
+use Cpanel::Config::LoadCpUserFile ();
+foreach my $user ( Cpanel::Config::Users::getcpusers() ) {
+    my $cpuser_ref = Cpanel::Config::LoadCpUserFile::load($user);
+    my $ip         = $cpuser_ref->{'IP'};
+    my $domains    = join( ' ', map { qq{"$_"} } ( $cpuser_ref->{'DOMAIN'}, @{ $cpuser_ref->{'DOMAINS'} } ) );
+    next if !$domains || !$ip;
+    print qq{/usr/local/cpanel/bin/swapip -1 "$ip" "$ip" $domains\n};
+}

4eafc0099c8054c10942e5b363c641233c04a0b6
Razvan Rosca <razvan.rosca@gmail.com>
2017-07-07 21:28:30 +0300

    Update centos-manage.sh

--- firewall/centos-manage.sh
+++ firewall/centos-manage.sh
@@ -1,1 +1,5 @@
-firewall-cmd --zone=public --permanent --add-port=65432/tcp && firewall-cmd --reload
+read -p "Zone: " zone;
+read -p "Port: " port;
+read -p "Protocol (tcp/udp): " protocol;
+printf %s "Opening port $port, protocol $protocol on zone $zone..."
+firewall-cmd --zone=$zone --permanent --add-port=$port/$protocol && firewall-cmd --reload

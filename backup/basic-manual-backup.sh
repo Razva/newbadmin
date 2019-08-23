8dd06d2c51489fa8062b2a511379836a4a1f2814
Razvan Rosca <razvan.rosca@gmail.com>
2019-08-24 00:01:25 +0300

    Create basic-manual-backup.sh

---
+++ backup/basic-manual-backup.sh
@@ -0,0 +1,20 @@
+### Edit Variables ###
+backup_location="/home/user/backup" # Don't use relative paths
+sql_user="my_sql_user"
+sql_db="my_sql_database"
+sql_pass="MySuperSecretPassword"
+backup_directories="/home/user/dir1 /home/user/dir2" # Separate multiple directories by space
+### Stop Editing ###
+
+now=$(date +"%m_%d_%Y")
+echo "Checking & Creating backup location..."
+mkdir -p $backup_location
+echo "Dumping SQL..."
+mysqldump -u $sql_user -p$sql_pass $sql_db > $backup_location/$sql_db-$now.sql
+echo "Compressing SQL..."
+cd $backup_location && tar -czf $sql_db-$now-sql.tar.gz $sql_db-$now.sql
+echo "Cleaning up SQL..."
+rm -rf $backup_location/$sql_db-$now.sql
+echo "Compressing directories..."
+cd $backup_location && tar -czf files-$now.tar.gz $backup_directories
+echo "DONE!"

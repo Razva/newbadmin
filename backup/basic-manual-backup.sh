a1f85e14d3d2df685a5c4eecbd6444599455c081
Razvan Rosca <razvan.rosca@gmail.com>
2019-08-26 19:50:52 +0300

    Update basic-manual-backup.sh

--- backup/basic-manual-backup.sh
+++ backup/basic-manual-backup.sh
@@ -8,13 +8,13 @@ backup_directories="/home/user/dir1 /home/user/dir2" # Separate multiple directo
 
 now=$(date +"%m_%d_%Y")
 echo "Checking & Creating backup location..."
-mkdir -p $backup_location
+mkdir -p $backup_location/$now
 echo "Dumping SQL..."
-mysqldump -u $sql_user -p$sql_pass $sql_db > $backup_location/$sql_db-$now.sql
+mysqldump -u $sql_user -p$sql_pass $sql_db > $backup_location/$now/$sql_db-$now.sql
 echo "Compressing SQL..."
-cd $backup_location && tar -czf $sql_db-$now-sql.tar.gz $sql_db-$now.sql
+cd $backup_location/$now && tar -czf $sql_db-$now-sql.tar.gz $sql_db-$now.sql
 echo "Cleaning up SQL..."
-rm -rf $backup_location/$sql_db-$now.sql
+rm -rf $backup_location/$now/$sql_db-$now.sql
 echo "Compressing directories..."
-cd $backup_location && tar -czf files-$now.tar.gz $backup_directories
+cd $backup_location/$now/ && tar -czf files-$now.tar.gz $backup_directories
 echo "DONE!"

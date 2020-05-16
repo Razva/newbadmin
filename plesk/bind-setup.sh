8d42f765b35093de2787226b5e43f3f815baec01
Razvan Rosca <razvan.rosca@gmail.com>
2020-05-16 19:52:31 +0300

    Update bind-setup.sh

--- plesk/bind-setup.sh
+++ plesk/bind-setup.sh
@@ -8,7 +8,7 @@ log() {
 }
 
 log 'Adding SELinux rules ...'
-setsebool -P named_write_master_zones 1 &&
+setsebool -P named_write_master_zones 1
 log 'Done!'
 
 log 'Installing BIND ...'
@@ -16,12 +16,21 @@ yum -y install bind bind-utils &&
 log 'Done!'
 
 log 'Adding FirewallD rules ...'
-firewall-cmd --permanent --zone=public --add-port=53/tcp &&
-firewall-cmd --permanent --zone=public --add-port=53/udp &&
-firewall-cmd --permanent --zone=public --add-port=953/tcp &&
+firewall-cmd --permanent --zone=public --add-port=53/tcp
+firewall-cmd --permanent --zone=public --add-port=53/udp
+firewall-cmd --permanent --zone=public --add-port=953/tcp
 log 'Done!'
 
 log 'Setting BIND ...'
 mv /etc/named.conf /etc/named.conf.orig
 wget -O /etc/named.conf https://raw.githubusercontent.com/Razva/newbadmin/master/plesk/named.conf
 chown root:named /etc/named.conf*
+log 'Done!'
+
+log 'Enabling and Starting BIND ...'
+systemctl enable bind
+systemctl stop bind
+systemctl start bind
+log 'Done!'
+log ''
+log '!!! REMEMBER TO SETUP PLESK IP AND KEY IN NAMED.CONF !!!'

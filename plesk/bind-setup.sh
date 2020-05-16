3db7ae8eae7fbad401f9847938496a9416084444
Razvan Rosca <razvan.rosca@gmail.com>
2020-05-16 19:49:46 +0300

    Create bind-setup.sh

---
+++ plesk/bind-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+log() {
+  local green=$(tput setaf 2)
+  local normal=$(tput sgr0)
+  printf "${green}=== %s ${normal}\n" "$1"
+}
+
+log 'Adding SELinux rules ...'
+setsebool -P named_write_master_zones 1 &&
+log 'Done!'
+
+log 'Installing BIND ...'
+yum -y install bind bind-utils &&
+log 'Done!'
+
+log 'Adding FirewallD rules ...'
+firewall-cmd --permanent --zone=public --add-port=53/tcp &&
+firewall-cmd --permanent --zone=public --add-port=53/udp &&
+firewall-cmd --permanent --zone=public --add-port=953/tcp &&
+log 'Done!'
+
+log 'Setting BIND ...'
+mv /etc/named.conf /etc/named.conf.orig
+wget -O /etc/named.conf https://raw.githubusercontent.com/Razva/newbadmin/master/plesk/named.conf
+chown root:named /etc/named.conf*

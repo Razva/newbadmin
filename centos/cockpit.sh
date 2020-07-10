e40532f5cd706770626002c6645b815b75f639b4
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-10 16:58:52 +0300

    Update cockpit.sh

--- centos/cockpit.sh
+++ centos/cockpit.sh
@@ -1,4 +1,22 @@
-dnf install cockpit &&
-systemctl enable --now cockpit.socket &&
+#!/bin/bash
+
+log() {
+  local green=$(tput setaf 2)
+  local normal=$(tput sgr0)
+  printf "${green}=== %s ${normal}\n" "$1"
+}
+
+log 'Updating OS ...'
+yum -y update
+log 'Done!'
+echo ""
+log 'Installing Cockpit'
+yum -y install cockpit
+systemctl enable --now cockpit.socket
+log 'Done!'
+echo ""
+log 'Setting firewall ...'
 firewall-cmd --permanent --zone=public --add-service=cockpit &&
 firewall-cmd --reload
+log 'Done!'
+rm -rf cockpit.sh

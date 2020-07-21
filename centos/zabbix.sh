8400df1092c235d511bfc4cbab9ba917b9a8d0a1
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-21 12:35:04 +0300

    Update zabbix.sh

--- centos/zabbix.sh
+++ centos/zabbix.sh
@@ -15,17 +15,17 @@ while true; do
 	log 'Selecting OS ...'
 	read -p 'Select CentOS Version (7/8): ' os;
 	echo ""
-		case $os in
+	case $os in
 		7) log 'Installing CentOS 7 Agent...'
 			rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/7/x86_64/zabbix-release-5.0-1.el7.noarch.rpm
-    	yum clean all
-    	yum install zabbix-agent
-    ;;
+    			yum clean all
+    			yum install zabbix-agent
+   		;;
 		8) log 'Installing CentOS 8 Agent...'
 			rpm -Uvh https://repo.zabbix.com/zabbix/5.0/rhel/8/x86_64/zabbix-release-5.0-1.el8.noarch.rpm
 			dnf clean all
 			dnf install zabbix-agent
-    ;;
+		;;
 		*) log 'Please choose either 7 or 8.'
 			echo ""
 		;;

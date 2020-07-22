d39051c073fc53de88d5d2ed41bd016fb54447c4
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-22 20:01:57 +0300

    Update centos.sh

--- centos/centos.sh
+++ centos/centos.sh
@@ -23,8 +23,8 @@ echo -e "CentOS Version: \e[1m\e[91m$(rpm -E %{rhel})\e[0m"
 echo -e "Hostname: \e[1m\e[91m$(hostname)\e[0m"
 echo ""
 while true; do
-	read -p 'Select CentOS Version (7/8): ' os;
-	case $os in
+	read -p 'Select CentOS Version (7/8): ' OS;
+	case $OS in
 		7) yum -y install wget nano screen tar unzip epel-release
       	 	;;
 		8) dnf -y install wget nano screen tar unzip epel-release
@@ -37,8 +37,8 @@ log 'Done!'
 echo ""
 
 log 'Setting Time ...'
-	read -r -p "Would you like to Set and Sync Time? [Y/N] " time
-	case "$time" in
+	read -r -p "Would you like to Set and Sync Time? [Y/N] " TIME
+	case "$TIME" in
 		[yY][eE][sS]|[yY]) log "Setting Time ..."
 		timedatectl set-timezone Europe/Bucharest
 		yum -y install chrony
@@ -50,9 +50,9 @@ log 'Done!'
 echo ""
 
 log 'Setting SSHD and FirewallD ...'
-	read -r -p "Would you like to setup a custom SSH Port? [Y/N] " sshd
-	case "$sshd" in
-		[yY][eE][sS]|[yY]) read -p 'Define custom SSH Port: ' sshport;
+	read -r -p "Would you like to setup a custom SSH Port? [Y/N] " SSHD
+	case "$SSHD" in
+		[yY][eE][sS]|[yY]) read -p 'Define custom SSH Port: ' SSHPORT;
 			echo ""
 			
 			log 'Installing FirewallD ...'
@@ -62,12 +62,12 @@ log 'Setting SSHD and FirewallD ...'
 			echo ""
 			
 			log 'Setting ports ...'
-			firewall-cmd --zone=public --permanent --add-port=$sshport/tcp
+			firewall-cmd --zone=public --permanent --add-port=$SSHPORT/tcp
 			firewall-cmd --zone=public --permanent --add-port=80/tcp
 			firewall-cmd --zone=public --permanent --add-port=443/tcp
-			if [[ "$os" -eq 7 ]]
+			if [[ "$OS" -eq 7 ]]
 				//do7
-			if [[ "$os" -eq 8 ]]
+			if [[ "$OS" -eq 8 ]]
 				firewall-cmd --zone=public --permanent --remove-service=cockpit
 				firewall-cmd --zone=public --permanent --remove-service=dhcpv6-client
 			fi
@@ -77,17 +77,17 @@ log 'Setting SSHD and FirewallD ...'
 			echo ""
 
 			log 'Adding SELinux SSH Port Rule ...'
-			if [[ "$os" -eq 7 ]]
+			if [[ "$OS" -eq 7 ]]
 				yum -y install policycoreutils-python
-			if [[ "$os" -eq 8 ]]
+			if [[ "$OS" -eq 8 ]]
 				dnf -y install policycoreutils-python-utils
 			fi
-			semanage port -a -t ssh_port_t -p tcp $sshport
+			semanage port -a -t ssh_port_t -p tcp $SSHPORT
 			log 'Done!'
 			echo ""
 
 			log 'Changing SSHD Port ...'
-			sed -i 's/^#Port 22$/Port "${sshport}"/g' /etc/ssh/sshd_config
+			sed -i 's/^#Port 22$/Port "${SSHPORT}"/g' /etc/ssh/sshd_config
 			log 'Done!'
 			echo ""
 
@@ -101,23 +101,23 @@ log 'SSHD and FirewallD - Done!'
 echo ""
 
 log 'Setting Sudoers ...'
-read -r -p "Would you like to add a SUDO user? [y/n] " sudo
-case "$sudo" in
+read -r -p "Would you like to add a SUDO user? [y/n] " SUDO
+case "$SUDO" in
 	[yY][eE][sS]|[yY]) log 'Setting up SUDO user ...'
-		read -p 'New user: ' user;
-		useradd $user
-		passwd $user
-		usermod -aG wheel $user
+		read -p 'New user: ' USER;
+		useradd $USER
+		passwd $USER
+		usermod -aG wheel $USER
 		log 'Done!'
 		echo ""
-		log "Adding SSH Key for $user ..."
-		rm -rf /home/$user/.ssh
-		mkdir -p /home/$user/.ssh
-		wget -O /home/$user/.ssh/authorized_keys https://t.isp.fun/authorized_keys
-		chmod 700 /home/$user/.ssh
-		chmod 600 /home/$user/.ssh/authorized_keys
-		chown -R $user:$user /home/$user/.ssh
-		restorecon -R -v /home/$user/.ssh
+		log "Adding SSH Key for $USER ..."
+		rm -rf /home/$USER/.ssh
+		mkdir -p /home/$USER/.ssh
+		wget -O /home/$USER/.ssh/authorized_keys https://t.isp.fun/authorized_keys
+		chmod 700 /home/$USER/.ssh
+		chmod 600 /home/$USER/.ssh/authorized_keys
+		chown -R $USER:$USER /home/$USER/.ssh
+		restorecon -R -v /home/$USER/.ssh
 		log 'Done!'
 		;;
 	esac

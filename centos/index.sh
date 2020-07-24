ff72b56055614abcc5be3b1edb6043b86cab476e
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-24 11:49:42 +0300

    Create index.sh

---
+++ centos/index.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+# Color & Style
+log() {
+	local green=$(tput setaf 2)
+	local normal=$(tput sgr0)
+	printf "${green}=== %s ${normal}\n" "$1"
+}
+
+red=$(tput setaf 1)
+normal=$(tput sgr0)
+
+# General Information
+log 'System information ...'
+printf "CentOS Version: ${red} rpm -E %{rhel} ${normal}"
+
+
+
+
+echo -e "Current Hostname: \e[1m\e[91m$(hostname)\e[0m"
+echo -e "SELinux: \e[1m\e[91m$(cat /etc/selinux/config | grep "^SELINUX=")\e[0m"
+echo -e "Current Date & Time: \e[1m\e[91m$ (date '+%H:%M:%S'| xargs echo -n; timedatectl | grep 'Time zone') \e[0m"|sed -E 's/ +/ /g'

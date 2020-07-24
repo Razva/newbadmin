8f04b8aef30d8103a895f0f64a8812e7ead69160
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-25 01:27:01 +0300

    Update index.sh

--- centos/index.sh
+++ centos/index.sh
@@ -7,36 +7,35 @@ green=$'\e[92m'
 yellow=$'\e[93m'
 normal=$'\e[0m'
 
-log() {
-        printf '%s=== %s%s\n' "$green" "$1" "$normal"
-}
+h1() { printf '\n%s=== %s%s' "$green" "$1" "$normal"; }
+h2() { printf '%s= %s%s\n' "$yellow" "$1" "$normal"; }
 
 # General Information
 clear
-log 'System information ...'
-printf 'CentOS Version: %s\n' "$red$(rpm -E %{rhel})$normal"
+h1 'System information ...'
+printf '\nCentOS Version: %s\n' "$red$(rpm -E %{rhel})$normal"
 printf 'Current Hostname: %s\n' "$red$(hostname) $normal"
 printf 'SELinux Status: %s\n' "$red$(grep "^SELINUX=" /etc/selinux/config) $normal"
 printf 'Current Date & Time: %s\n' "$red$(date '+%H:%M:%S') |$(timedatectl | grep 'Time zone' | sed -E 's/ +/ /g') $normal"
 
-printf '%s\nRemove Cockpit? [Y/n] %s' "$green" "$normal" ; read -r ICOCKPIT
+h1 'Remove Cockpit? [Y/n] ' ; read -r ICOCKPIT
 case ${ICOCKPIT,,} in
         [yY][eE][sS]|[yY]|"")
                 wget --quiet https://raw.githubusercontent.com/Razva/newbadmin/master/centos/cockpit.sh
                 source ./cockpit.sh
                 rm -rf ./cockpit.sh
                 ;;
-        *) printf '%s\n' "${green}Skipping Cockpit ...${normal}";;
+        *) h2 'Skipping Cockpit ...'
 esac
 
-printf '%s\nUpdate OS? [Y/n] %s' "$green" "$normal" ; read -r IUPDATE
+h1 'Update OS? [Y/n] ' ; read -r IUPDATE
 case ${IUPDATE,,} in
         [yY][eE][sS]|[yY]|"")
                 wget --quiet https://raw.githubusercontent.com/Razva/newbadmin/master/centos/update.sh
                 source ./update.sh
                 rm -rf ./update.sh
                 ;;
-        *) printf '%s\n' "${green}Skipping OS Updates ...${normal}";;
+        *) h2 'Skipping OS Updates ...';
 esac
 
-printf '\nScript END!\n\n'
+printf '\nEND!\n\n'

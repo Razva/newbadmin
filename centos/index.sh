87767285da577057c851a0a6df982cd7346ca4d0
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-24 21:22:43 +0300

    Update index.sh

--- centos/index.sh
+++ centos/index.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
+!/bin/bash
 
 # Color & Style
 log() {
-	local green=$(tput setaf 2)
-	local normal=$(tput sgr0)
-	printf '%s=== %s%s\n' "$green" "$1" "$normal"
+        local green=$(tput setaf 2)
+        local normal=$(tput sgr0)
+        printf '%s=== %s%s\n' "$green" "$1" "$normal"
 }
 
-red=$(tput setaf 1)
+red=$'\e[91m'
 normal=$(tput sgr0)
 
 # General Information
 log 'System information ...'
-printf "CentOS Version: ${red} 'rpm -E %{rhel}' ${normal}"
-
-
-
-
-echo -e "Current Hostname: \e[1m\e[91m$(hostname)\e[0m"
-echo -e "SELinux: \e[1m\e[91m$(cat /etc/selinux/config | grep "^SELINUX=")\e[0m"
-echo -e "Current Date & Time: \e[1m\e[91m$ (date '+%H:%M:%S'| xargs echo -n; timedatectl | grep 'Time zone') \e[0m"|sed -E 's/ +/ /g'
+printf 'CentOS Version: %s\n' "$red$(rpm -E %{rhel})$normal"
+printf 'Current Hostname: %s\n' "$red$(hostname) $normal"
+printf 'SELinux Status: %s\n' "$red$(grep "^SELINUX=" /etc/selinux/config) $normal"
+printf 'Current Date & Time: %s\n' "$red$(date '+%H:%M:%S') |$(timedatectl | grep 'Time zone' | sed -E 's/ +/ /g') $normal"

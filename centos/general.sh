30892347dc5ae039625287795624b603775722d0
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-25 11:26:31 +0300

    Create general.sh

---
+++ centos/general.sh
@@ -0,0 +1,13 @@
+h1 'General settings ...'
+while true; do
+	h2 'Select CentOS Version (7/8): ' IVERSION;
+		case $IVERSION in
+			7) echo "7"
+			;;
+			8) echo "8"
+			;;
+			*) h3 'Please choose either 7 or 8.'
+			;;
+		esac
+	break
+done

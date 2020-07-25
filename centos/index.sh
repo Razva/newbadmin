d652d7791f597e3246f58846675f5d34f2426070
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-25 11:25:50 +0300

    Update index.sh

--- centos/index.sh
+++ centos/index.sh
@@ -22,6 +22,12 @@ printf 'Current Hostname: %s\n' "$red$(hostname) $normal"
 printf 'SELinux Status: %s\n' "$red$(grep "^SELINUX=" /etc/selinux/config) $normal"
 printf 'Current Date & Time: %s\n\n' "$red$(date '+%H:%M:%S') |$(timedatectl | grep 'Time zone' | sed -E 's/ +/ /g') $normal"
 
+# Setting general values
+curl -Os https://raw.githubusercontent.com/Razva/newbadmin/master/centos/general.sh
+source ./general.sh
+rm -rf ./general.sh
+
+# Cockpit
 h1 'Remove Cockpit? [Y/n] ' ; read -r ICOCKPIT
 case ${ICOCKPIT,,} in
         [yY][eE][sS]|[yY]|"")
@@ -32,6 +38,7 @@ case ${ICOCKPIT,,} in
         *) h2 $'Skipping Cockpit ...\n\n'
 esac
 
+# OS Updates
 h1 'Update OS? [Y/n] ' ; read -r IUPDATE
 case ${IUPDATE,,} in
         [yY][eE][sS]|[yY]|"")
@@ -42,6 +49,7 @@ case ${IUPDATE,,} in
         *) h2 $'Skipping OS Updates ...\n\n';
 esac
 
+# Hostname
 h1 'Set Hostname? [Y/n] ' ; read -r IHOSTNAME
 case ${IHOSTNAME,,} in
         [yY][eE][sS]|[yY]|"")
@@ -52,6 +60,7 @@ case ${IHOSTNAME,,} in
         *) h2 $'Skipping Hostname ...\n\n';
 esac
 
+# Time
 h1 'Set Time? [Y/n] ' ; read -r ITIME
 case ${ITIME,,} in
         [yY][eE][sS]|[yY]|"")

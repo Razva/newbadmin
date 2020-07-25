cfab3a5a4d8e71bf86c27ef9d1c84c66e2f9a566
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-25 11:32:46 +0300

    Update general.sh

--- centos/general.sh
+++ centos/general.sh
@@ -1,6 +1,6 @@
 h1 'General settings ...'
 while true; do
-	h2 'Select CentOS Version (7/8): ' IVERSION;
+	h2 'Select CentOS Version (7/8): ' ; read -r IVERSION
 		case $IVERSION in
 			7) echo "7"
 			;;

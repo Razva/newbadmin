664d8456ad0b779e2abf031385f717b9416c9ff8
Razvan Rosca <razvan.rosca@gmail.com>
2020-07-25 15:31:10 +0300

    Update general.sh

--- centos/general.sh
+++ centos/general.sh
@@ -2,17 +2,17 @@
 
 h1 $'General settings ...\n'
 while true; do
-	h2 'Select CentOS Version (7/8): ' ; read -r IVERSION
-		case $IVERSION in
-			7) OS=7
-			break
-			;;
-			8) OS=8
-			break
-			;;
-			*) h3 $'Please choose either 7 or 8.\n'
-			;;
-	esac
+        h2 'Select CentOS Version (7/8): ' ; read -r IVERSION
+                case $IVERSION in
+                        7) OS=7
+                        break
+                        ;;
+                        8) OS=8
+                        break
+                        ;;
+                        *) h3 $'Please choose either 7 or 8.\n'
+                        ;;
+        esac
 done
 
-h3 $'Selected CentOS $OS. Continuing ...\n'
+h3 "Selected CentOS $red$OS$normal. Continuing ..."$'\n\n'

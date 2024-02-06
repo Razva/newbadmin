db4d7e13428f73da4a391e20737a22327089b087
Razvan Rosca <razvan.rosca@gmail.com>
2024-02-06 14:00:39 +0200

    Update basic-manual-backup.sh

--- archive/backup/basic-manual-backup.sh
+++ archive/backup/basic-manual-backup.sh
@@ -4,17 +4,18 @@ sql_user="my_sql_user"
 sql_db="my_sql_database"
 sql_pass="MySuperSecretPassword"
 backup_directories="/home/user/dir1 /home/user/dir2" # Separate multiple directories by space
+excluded_directories="/home/user/excluded"
 ### Stop Editing ###
 
-now=$(date +"%m_%d_%Y")
+now=$(date +"%Y_%m_%d")
 echo "Checking & Creating backup location..."
 mkdir -p $backup_location/$now
 echo "Dumping SQL..."
-mysqldump -u $sql_user -p$sql_pass $sql_db > $backup_location/$now/$sql_db-$now.sql
+mysqldump -u $sql_user -p$sql_pass $sql_db > $backup_location/$now/sql-$sql_db-$now.sql
 echo "Compressing SQL..."
-cd $backup_location/$now && tar -czf $sql_db-$now-sql.tar.gz $sql_db-$now.sql
+cd $backup_location/$now && tar czfP sql-$sql_db-$now.tar.gz sql-$sql_db-$now.sql
 echo "Cleaning up SQL..."
-rm -rf $backup_location/$now/$sql_db-$now.sql
+rm -rf $backup_location/$now/sql-$sql_db-$now.sql
 echo "Compressing directories..."
-cd $backup_location/$now/ && tar -czf files-$now.tar.gz $backup_directories
+cd $backup_location/$now/ && tar czfP files-$now.tar.gz --exclude="$excluded_directories" $backup_directories
 echo "DONE!"
